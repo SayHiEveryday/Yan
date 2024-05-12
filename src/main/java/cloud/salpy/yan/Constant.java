278fe4cdade02a654364c4ecb198d07ac08d3083
SayHi <103105268+SayHiEveryday@users.noreply.github.com>
2024-05-12 14:47:17 +0700

    Delete src/main/java/cloud/salpy/yan/Constant.java

--- src/main/java/cloud/salpy/yan/Constant.java
+++
@@ -1,6 +0,0 @@
-package cloud.salpy.yan;
-
-public class Constant {
-    public static String prefix = ">";
-    public static String token = "MTE1NTQxODA0MzY3ODIxMjEwNw.GPCxF3.7PFiBBZaHaCiwHsYdfj3tfylbfvCp2jE3RoTTc";
-}
